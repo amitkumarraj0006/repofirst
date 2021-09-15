38135a614362691b652e44fc968f7e942ca3ce47
amit <amitkumarraj0006@gmail.com>
2021-09-15 15:14:45 +0000

    changes on feature101

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("hello to author change the first commit");
+System.out.println("hello to author changes on feature101");
 }
 }
 
